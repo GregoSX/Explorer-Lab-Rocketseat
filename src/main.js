67a7c73699b24ae1cd68c3d182d7b82b246ef821
Guilherme Grego <guilhermegrego376@gmail.com>
2022-10-20 10:43:40 -0300

    feat: Eventos

--- src/main.js
+++ src/main.js
@@ -74,3 +74,40 @@ const cardNumberPattern = {
     }
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', () => {
+    alert('Cartão adicionado')
+})
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+})
+
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+    const ccHolder = document.querySelector('.cc-holder .value')
+    
+    ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+    const ccSecurity = document.querySelector('.cc-security .value')
+
+    ccSecurity.innerText = securityCodeMasked.value.length === 0 ? "123" : securityCodeMasked.value
+})
+
+cardNumberMasked.on("accept", () => {
+    const cardType = cardNumberMasked.masked.currentMask.cardType
+    setCardType(cardType)
+   
+    const ccNumber = document.querySelector('.cc-number')
+
+    ccNumber.innerText = cardNumberMasked.value.length === 0 ? "1234 5678 9012 3456" : cardNumberMasked.value
+})
+
+expirationDateMasked.on("accept", () => {
+    const ccExpiration = document.querySelector('.cc-extra .value')
+
+    ccExpiration.innerText = expirationDateMasked.value.length === 0 ? "02/32" : expirationDateMasked.value
+})
